bfb40693b1d4257c80649c23016b5ce2356ba155
magesh <mageshp121@gmail.com>
2023-02-19 16:09:35 +0530

    finished

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+       <div className='blur' style={{top:'-18%',right:'0'}}></div>
+       <div className='blur' style={{top:'30%',right:'-8rem'}}></div>
+    </div>
+  );
+}
+
+export default App;
